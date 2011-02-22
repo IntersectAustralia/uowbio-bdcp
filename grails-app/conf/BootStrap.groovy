0d289ba16c7677a49c80c40be671206d702dbfa7
David Pollum <david@intersect.org.au>
2011-02-22 16:12:42 +1100

    Changed UrlMapping so that the first page lists the projects

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,16 @@
+import au.org.intersect.bdcp.Project
+
 class BootStrap {
 
     def init = { servletContext ->
+		def project = new Project(projectTitle: 'TestProject',
+				researcherName: 'TestStudent' ,
+				degree: 'TestDegree',
+				yearFrom: new Date(),
+				yearTo: new Date(),
+				description: 'Test Description',
+				supervisors: 'test supervisor')
+		project.save()
     }
     def destroy = {
     }
