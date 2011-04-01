d02a0fa8ad236623cd42ecae809dae1d98da292a
David Pollum <david@intersect.org.au>
2011-04-01 17:03:10 +1100

    Added environment aware code for resources.groovy

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,21 +1,36 @@
 
 
 // Place your Spring DSL code here
+
+import grails.util.*
 beans = {
-	
-	contextSource(org.springframework.ldap.core.support.LdapContextSource){
-			url="ldap://localhost:10400"
-			base="ou=people,dc=biomechanics, dc=local"
-			userDn="uid=admin,ou=system"
-			password="secret"
-		}
-	
-		myLdapAuthenticator(au.org.intersect.bdcp.ldap.MyLdapAuthenticator, ref("contextSource")) {
-			userDnPatterns = ['uid={0}']
-		}
+	switch(GrailsUtil.environment)
+	{
+		case "development":
+
+			contextSource(org.springframework.ldap.core.support.LdapContextSource)
+			{
+				url="ldap://localhost:10400"
+				base="ou=people,dc=biomechanics, dc=local"
+				userDn="uid=admin,ou=system"
+				password="secret"
+			}
+			break
+		case "production":
+			contextSource(org.springframework.ldap.core.support.LdapContextSource)
+			{
+				url="ldap://localhost:10400"
+				base="ou=people,dc=biomechanics, dc=local"
+				userDn="uid=admin,ou=system"
+				password="secret"
+			}
+			break
+	}
+	myLdapAuthenticator(au.org.intersect.bdcp.ldap.MyLdapAuthenticator, ref("contextSource"))
+	{ userDnPatterns = ['uid={0}'] }
 
 	// this overrides the default Authentication Provider with our authenticator and our user details service
 	myLdapAuthenticationProvider(org.springframework.security.ldap.authentication.LdapAuthenticationProvider,
-	   ref("myLdapAuthenticator"))
-	
+			ref("myLdapAuthenticator"))
+
 }
