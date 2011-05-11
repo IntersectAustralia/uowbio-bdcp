64ae6d4c9d62fd6d8e43bdf563a72bb8b04e6c67
David Pollum <david@intersect.org.au>
2011-05-11 16:35:22 +1000

    Updated error messages so that they are human readable and wrote unit tests for device grouping story

--- test/unit/au/org/intersect/bdcp/DeviceGroupTests.groovy
+++ test/unit/au/org/intersect/bdcp/DeviceGroupTests.groovy
@@ -2,16 +2,54 @@ package au.org.intersect.bdcp
 
 import grails.test.*
 
-class DeviceGroupTests extends GrailsUnitTestCase {
-    protected void setUp() {
+class DeviceGroupTests extends GrailsUnitTestCase 
+{
+    def deviceGroup
+    
+   /**
+    * Setup operations before each test
+    */
+    protected void setUp() 
+    {
         super.setUp()
+        deviceGroup = new DeviceGroup(groupingName:"TestGrouping")
+        mockForConstraintsTests DeviceGroup, [deviceGroup]
     }
-
-    protected void tearDown() {
+    
+    /**
+     * Cleanup operations after each test
+     */
+    protected void tearDown() 
+    {
         super.tearDown()
     }
 
-    void testSomething() {
+   /**
+    * Test that the blank fields in the domain class {@link DeviceGroup} are
+    * correctly validated
+    */
+    void testBlank() {
 
+        deviceGroup = new DeviceGroup(groupingName: '')
+        
+        assertFalse 'No validation for blank field(s)' ,deviceGroup.validate()
+        assertEquals 'Grouping Name is blank.','blank', deviceGroup.errors['groupingName']
+        deviceGroup = new DeviceGroup(groupingName: 'TestGrouping1')
+        assertTrue "A valid device grouping did not validate!", deviceGroup.validate()
     }
+    
+    /**
+    * Test that the blank fields in the domain class {@link DeviceGroup} are
+    * correctly validated
+    */
+    void testUnique() {
+
+        deviceGroup = new DeviceGroup(groupingName: 'TestGrouping')
+        
+        assertFalse 'No validation for unique field(s)' ,deviceGroup.validate()
+        assertEquals 'Grouping Name is not unique.','unique', deviceGroup.errors['groupingName']
+        deviceGroup = new DeviceGroup(groupingName: 'TestGrouping2')
+        assertTrue "A valid device grouping did not validate!", deviceGroup.validate()
+    }
+    
 }
