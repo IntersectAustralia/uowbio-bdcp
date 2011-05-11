3e570251f45ca493d67ecdbd9f96c70efa184366
David Pollum <david@intersect.org.au>
2011-05-12 09:55:20 +1000

    Removed unnecessary code from SessionFileController

--- grails-app/controllers/au/org/intersect/bdcp/SessionFileController.groovy
+++ grails-app/controllers/au/org/intersect/bdcp/SessionFileController.groovy
@@ -54,7 +54,7 @@ class SessionFileController
 	def index =
 	{
 		cache false
-        redirect(action: "list", params: params)
+        redirect(action: "fileList", params: params)
 	}
 	
 	@Secured(['IS_AUTHENTICATED_REMEMBERED'])
@@ -77,12 +77,6 @@ class SessionFileController
 		}
 		[componentInstanceList: Component.findAllByStudy(studyInstance), componentInstanceTotal: Component.countByStudy(studyInstance), studyInstance: studyInstance, sessionFiles: sessionFiles]
 	}
-	
-	@Secured(['IS_AUTHENTICATED_REMEMBERED'])
-	def uploadFiles =
-	{
-	    cache false
-    }
 
 	@Secured(['IS_AUTHENTICATED_REMEMBERED'])
 	def createDirectory =
@@ -134,130 +128,7 @@ class SessionFileController
         
         ['path': path]
 	}
-
-	@Secured(['IS_AUTHENTICATED_REMEMBERED'])
-	def list =
-	{
-		params.max = Math.min(params.max ? params.int('max') : 10, 100)
-		[sessionFileInstanceList: SessionFile.list(params), sessionFileInstanceTotal: SessionFile.count()]
-	}
-
-	@Secured(['IS_AUTHENTICATED_REMEMBERED'])
-	def create =
-	{
-		def sessionFileInstance = new SessionFile()
-		sessionFileInstance.properties = params
-		return [sessionFileInstance: sessionFileInstance]
-	}
-
-	@Secured(['IS_AUTHENTICATED_REMEMBERED'])
-	def save =
-	{
-		def sessionFileInstance = new SessionFile(params)
-		if (sessionFileInstance.save(flush: true))
-		{
-			flash.message = "${message(code: 'default.created.message', args: [message(code: 'sessionFile.label', default: 'SessionFile'), sessionFileInstance.id])}"
-			redirect(action: "show", id: sessionFileInstance.id)
-		}
-		else
-		{
-			render(view: "create", model: [sessionFileInstance: sessionFileInstance])
-		}
-	}
-
-	@Secured(['IS_AUTHENTICATED_REMEMBERED'])
-	def show =
-	{
-		def sessionFileInstance = SessionFile.get(params.id)
-		if (!sessionFileInstance)
-		{
-			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'sessionFile.label', default: 'SessionFile'), params.id])}"
-			redirect(action: "list")
-		}
-		else
-		{
-			[sessionFileInstance: sessionFileInstance]
-		}
-	}
-
-	@Secured(['IS_AUTHENTICATED_REMEMBERED'])
-	def edit =
-	{
-		def sessionFileInstance = SessionFile.get(params.id)
-		if (!sessionFileInstance)
-		{
-			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'sessionFile.label', default: 'SessionFile'), params.id])}"
-			redirect(action: "list")
-		}
-		else
-		{
-			return [sessionFileInstance: sessionFileInstance]
-		}
-	}
-
-	@Secured(['IS_AUTHENTICATED_REMEMBERED'])
-	def update =
-	{
-		def sessionFileInstance = SessionFile.get(params.id)
-		if (sessionFileInstance)
-		{
-			if (params.version)
-			{
-				def version = params.version.toLong()
-				if (sessionFileInstance.version > version)
-				{
-
-					sessionFileInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [
-						message(code: 'sessionFile.label', default: 'SessionFile')]
-					as Object[], "Another user has updated this SessionFile while you were editing")
-					render(view: "edit", model: [sessionFileInstance: sessionFileInstance])
-					return
-				}
-			}
-			sessionFileInstance.properties = params
-			if (!sessionFileInstance.hasErrors() && sessionFileInstance.save(flush: true))
-			{
-				flash.message = "${message(code: 'default.updated.message', args: [message(code: 'sessionFile.label', default: 'SessionFile'), sessionFileInstance.id])}"
-				redirect(action: "show", id: sessionFileInstance.id)
-			}
-			else
-			{
-				render(view: "edit", model: [sessionFileInstance: sessionFileInstance])
-			}
-		}
-		else
-		{
-			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'sessionFile.label', default: 'SessionFile'), params.id])}"
-			redirect(action: "list")
-		}
-	}
-
-	@Secured(['IS_AUTHENTICATED_REMEMBERED'])
-	def delete =
-	{
-		def sessionFileInstance = SessionFile.get(params.id)
-		if (sessionFileInstance)
-		{
-			try
-			{
-				sessionFileInstance.delete(flush: true)
-				flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'sessionFile.label', default: 'SessionFile'), params.id])}"
-				redirect(action: "list")
-			}
-			catch (org.springframework.dao.DataIntegrityViolationException e)
-			{
-				flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'sessionFile.label', default: 'SessionFile'), params.id])}"
-				redirect(action: "show", id: params.id)
-			}
-		}
-		else
-		{
-			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'sessionFile.label', default: 'SessionFile'), params.id])}"
-			redirect(action: "list")
-		}
-	}
 }
-
 class directoryCommand
 {
 	String name
