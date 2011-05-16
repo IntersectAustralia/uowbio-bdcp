8f0df0cb08e4692b19b25c3481c3441ca65b29d9
David Pollum <david@intersect.org.au>
2011-05-16 16:15:36 +1000

    Added unique validation to device and deviceGroup so that values are case sensitive unique

--- grails-app/domain/au/org/intersect/bdcp/Device.groovy
+++ grails-app/domain/au/org/intersect/bdcp/Device.groovy
@@ -18,7 +18,7 @@ class Device {
     
     static constraints = 
     {
-        name(blank:false, size:1..1000, unique:true)
+        name(blank:false, size:1..1000, trulyunique:true)
         description(blank:false, size:1..1000)
         manufacturer(blank:false, size:1..1000)
         locationOfManufacturer(size:1..1000)
