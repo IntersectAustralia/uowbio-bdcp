7aae6d62beb1211d0b9eace79f7650c097dba0aa
David Pollum <david@intersect.org.au>
2011-06-30 15:46:11 +1000

    Fixed problem with date not being remembered and issue when date is cleared that it was not being cleared correctly

--- grails-app/taglib/au/org/intersect/bdcp/JqueryDatePickerTagLib.groovy
+++ grails-app/taglib/au/org/intersect/bdcp/JqueryDatePickerTagLib.groovy
@@ -9,17 +9,27 @@ def jqDatePicker = {attrs, body ->
     def name = attrs.name 
     def id = attrs.id ?: name
     def value = attrs?.value
-    
+    def dateValue
+    def dayValue
+    def monthValue
+    def yearValue
     if (value != null)
     {
         if (value instanceof LocalDate)
         {
-            value = value.getLocalMillis()
+            dateValue = value.getLocalMillis()
+            dayValue = value.getDayOfMonth()
+            monthValue = value.getMonthOfYear()
+            yearValue = value.getYear()
         }
         else if (value instanceof Date)
         {
-            value = value.getTime()
+            dateValue = value.getTime()
+            dayValue = value.toCalendar().get(Calendar.DATE)
+            monthValue = value.toCalendar().get(Calendar.MONTH)
+            yearValue = value.toCalendar().get(Calendar.YEAR)
         }
+        
     }
     
     def jqName = name.replace('.','\\\\.')
@@ -27,25 +37,33 @@ def jqDatePicker = {attrs, body ->
     jqName = jqName.replace(']','\\\\]')
     
     //Create date text field and supporting hidden text fields need by grails
-out.println "<input type=\"text\" name=\"${name}\" id=\"${id}\" />"
-out.println "<input type=\"hidden\" name=\"${name}_day\" id=\"${id}_day\" />"
-out.println "<input type=\"hidden\" name=\"${name}_month\" id=\"${id}_month\" />"
-out.println "<input type=\"hidden\" name=\"${name}_year\" id=\"${id}_year\" />"
 
-//Code to parse selected date into hidden fields required by grails
+    out.println "<input type=\"text\" name=\"${name}\" id=\"${id}\"  />"
+    out.println "<input type=\"hidden\" name=\"${name}_day\" id=\"${id}_day\" value=\"${dayValue}\" />"
+    out.println "<input type=\"hidden\" name=\"${name}_month\" id=\"${id}_month\" value=\"${monthValue}\" />"
+    out.println "<input type=\"hidden\" name=\"${name}_year\" id=\"${id}_year\" value=\"${yearValue}\" />"
+
+    //Code to parse selected date into hidden fields required by grails
 out.println "<script type=\"text/javascript\"> \$(document).ready(function(){"
 out.println "\$(\"#${jqName}\").datepicker({"
 out.println "changeMonth: true,"
 out.println "changeYear: true,"
 out.println "onClose: function(dateText, inst) {"
+out.println "if (dateText.length != 0){"
 out.println "\$(\"#${jqName}_month\").attr(\"value\",new Date(dateText).getMonth() +1);"
 out.println "\$(\"#${jqName}_day\").attr(\"value\",new Date(dateText).getDate());"
 out.println "\$(\"#${jqName}_year\").attr(\"value\",new Date(dateText).getFullYear());"
 out.println "}"
+out.println "else{"
+out.println "\$(\"#${jqName}_month\").attr(\"value\",'');"
+out.println "\$(\"#${jqName}_day\").attr(\"value\",'');"
+out.println "\$(\"#${jqName}_year\").attr(\"value\",'');"
+out.println "}"
+out.println "}"
 out.println "});"
-if (value != null)
+if (dateValue != null)
 {
-    out.println "\$(\"#${jqName}\").datepicker(\"setDate\", new Date(${value}) );"
+    out.println "\$(\"#${jqName}\").datepicker(\"setDate\", new Date(${dateValue}) ).trigger('change');"
 }
 out.println "})</script>"
 
