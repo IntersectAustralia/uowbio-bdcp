73cc01c57b6c9a8d41ef60e4f7d4c4204d4ad61c
Carlos Aya <carlos@intersect.org.au>
2011-07-01 11:08:19 +1000

    Added identifier for RIF-CS to UserStore.

--- grails-app/domain/au/org/intersect/bdcp/UserStore.groovy
+++ grails-app/domain/au/org/intersect/bdcp/UserStore.groovy
@@ -6,11 +6,12 @@ class UserStore{
 	boolean deactivated
     String authority
 
-    // String nlaIndentifier // RIF-CS
+    String nlaIdentifier // RIF-CS
 	
     static constraints = {
     	username(blank:false, unique:true, size:1..1000)
 		authority(nullable:false, blank:false)
+		nlaIdentifier(nullable:true, blank:false)
 	}
 	
 	String toString() {
