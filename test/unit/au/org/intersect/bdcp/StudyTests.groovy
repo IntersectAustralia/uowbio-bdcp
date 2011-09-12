0126b98ecf08f2a417e7ee2e30372d37bcc90e7b
Karl Herrmann <kherrman@uow.edu.au>
2011-09-12 13:23:49 +1000

    tagging 0.15.6

--- test/unit/au/org/intersect/bdcp/StudyTests.groovy
+++ test/unit/au/org/intersect/bdcp/StudyTests.groovy
@@ -73,7 +73,6 @@ class StudyTests extends GrailsUnitTestCase
 
 		assertEquals 'Study Title is blank.','blank', study.errors['studyTitle']
 		assertEquals 'Description is blank.','blank', study.errors['description']
-		assertNull 'Keywords field is null.', study.errors['keywords']
 		
 		study = new Study(studyTitle: 'Testing Study',
 				uowEthicsNumber: '110680' ,
